ee74890d360d9847015c955f63beb9dce6a2a9bb
AlePascal-Front <alejandro.padilla196@gmail.com>
2026-02-26 00:24:34 -0600

    file rename to follow project requirements.

--- src/page/index.js
+++ src/page/index.js
@@ -8,7 +8,7 @@ import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
+import PopupWithForm from "../components/PopupWithForms.js";
 import userInfo from "../components/UserInfo.js";
 
 const state = {
