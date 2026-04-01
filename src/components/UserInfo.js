0f87dc4871b5fe648dba2aa95deb12a4a2e81a50
AlePascal-Front <alejandro.padilla196@gmail.com>
2026-04-01 02:31:07 -0600

    Implemented getters and setters, implemented a method to rapidly change user avatar and also changed design pattern for this class to a singleton for consistency across app.

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,21 +1,42 @@
 import { DOM } from "../utils/dom.js";
-const { profile } = DOM;
+const profile = DOM.profile;
 
-export default class UserInfo {
-  constructor({ name, about }) {
+class UserInfo {
+  constructor({ name, about, avatar }) {
     this._name = name;
     this._about = about;
+    this._avatar = avatar;
   }
 
   getUserInfo() {
     return {
       name: this._name,
       about: this._about,
+      avatar: this._avatar,
     };
+  }
+
+  setName(name) {
+    this._name = name;
+  }
+
+  setAbout(about) {
+    this._about = about;
+  }
+
+  setAvatar(avatar) {
+    this._avatar = avatar;
   }
 
   setUserInfo() {
     profile.querySelector(".profile__name").textContent = this._name;
     profile.querySelector(".profile__description").textContent = this._about;
   }
+
+  setUserAvatar() {
+    profile.querySelector(".profile__photo").src = this._avatar;
+  }
 }
+
+const userInfo = new UserInfo({ name: null, about: null, avatar: null });
+export default userInfo;
